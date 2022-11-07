0b8e40973be0538f0d802f5fdc6a9f1bde151168
JannesSP <janspang@gmx.de>
2022-11-07 17:23:58 +0100

    translate fastas or sequences in terminal

--- src/complement.py
+++ src/complement.py
@@ -4,6 +4,9 @@
 # website: https://jannessp.github.io
 
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
+import os
+from Bio import SeqIO
+from Bio.Seq import Seq
 
 COMPLEMENT = {
     'A':'T',
@@ -31,25 +34,49 @@ def complement(seq):
 
 def parse() -> Namespace:
     parser = ArgumentParser(
-        formatter_class=ArgumentDefaultsHelpFormatter
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        add_help='Translate nucleotide sequences to its complement'
     )
-    parser.add_argument('sequence')
-    parser.add_argument('--reverse', action='store_true')
+    parser.add_argument('sequences', help='Input sequence separated with "," or fasta file')
+    parser.add_argument('--reverse', action='store_true', help='Use to print 3\'->5\' sequence.')
     return parser.parse_args()
 
 def main() -> None:
     args = parse()
-    inp = args.sequence
-    print('Input')
-    print(f"5'{' ' * (len(inp)-3)}3'")
-    print(inp + '\n')
-    c = complement(inp)
-    if args.reverse:
-        print(f"5'{' ' * (len(c)-3)}3'")
-        print(c[::-1])
+    inp : str = args.sequences
+    rev : bool = args.reverse
+
+    if inp.endswith('.fa') or inp.endswith('.fasta'):
+        assert os.path.exists(inp) and os.path.isfile(inp)
+        outfile = os.path.join(f'{os.path.splitext(inp)[0]}_reverse-complement{os.path.splitext(inp)[1]}') if rev else os.path.join(f'{os.path.splitext(inp)[0]}_complement{os.path.splitext(inp)[1]}')
+        records = []
+
+        for rec in SeqIO.parse(inp, 'fasta'):
+            c = complement(rec.seq)
+            rec.seq = Seq(c[::-1]) if rev else Seq(c)
+            rec.id += '_reverse-complement' if rev else '_complement'
+            records.append(rec)
+            rec.description = ''
+
+        SeqIO.write(records, outfile, 'fasta')
+
     else:
-        print(f"3'{' ' * (len(c)-3)}5'")
-        print(c)
+        for seq in inp.split(','):
+
+            print('Input')
+            print(f"5'{' ' * (len(seq)-3)}3'")
+            print(seq + '\n')
+
+            c = complement(seq)
+            if rev:
+                print(f"5'{' ' * (len(c)-3)}3'")
+                c = c[::-1]
+            else:
+                print(f"3'{' ' * (len(c)-3)}5'")
+            print(c)
+
+            if c == seq:
+                print('PALINDROM!')
 
 if __name__ == '__main__':
     main()
