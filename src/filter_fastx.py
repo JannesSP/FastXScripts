c2caca7d1324c7c785198c27acca239450be8279
Jannes Spangenberg <janspang@gmx.de>
2023-04-25 10:29:51 +0200

    Cleanup & Bug Fixes

--- src/filter_fastx.py
+++ src/filter_fastx.py
@@ -7,6 +7,7 @@ from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 from io import TextIOWrapper
 from Bio import SeqIO
 import numpy as np
+from os.path import exists
 
 def parse() -> Namespace:
     parser = ArgumentParser(
@@ -14,12 +15,12 @@ def parse() -> Namespace:
         description='Filter FASTA or FASTQ file for ids or length of reads'
     )
 
-    parser.add_argument('inFASTX', metavar='FASTX', type=str, help='Multi FASTQ or FASTA file')
+    parser.add_argument('inFASTX', metavar='inFASTX', type=str, help='Multi FASTQ or FASTA file')
+    parser.add_argument('outFASTX', metavar='outFASTX', type=str, help='FASTQ or FASTA file containing provided reads')
     mode = parser.add_mutually_exclusive_group(required = True)
     mode.add_argument('-i', '--read_ids', metavar='IDS', type=str, default=None, help='One read ID per line in file, line separated read IDs')
     mode.add_argument('-l', '--long', metavar='LENGTH', type=int, default=None, help='Filter FASTA or FASTQ file for reads given length or longer')
     mode.add_argument('-s', '--short', metavar='LENGTH', type=int, default=None, help='Filter FASTA or FASTQ file for reads given length or shorter')
-    parser.add_argument('-o', '--outFASTX', metavar='FASTX', type=str, default = None, help='FASTQ or FASTA file containing provided reads', required=True)
     nt_mode = parser.add_mutually_exclusive_group()
     nt_mode.add_argument('--dna', action='store_true', default=False, help='Convert output sequences to dna (ACGT)')
     nt_mode.add_argument('--rna', action='store_true', default=False, help='Convert output sequences to rna (ACGU)')
@@ -39,23 +40,38 @@ def main() -> None:
 
     print('Filtering', inFX)
 
-    if outFX.lower().endswith('.fa') or outFX.lower().endswith('.fasta'):
-        format = 'fasta'
-    elif outFX.lower().endswith('.fq') or outFX.lower().endswith('.fastq'):
-        format = 'fastq'
+    # Check Parameters
+    if inFX.lower().endswith('.fa') or inFX.lower().endswith('.fasta'):
+        informat = 'fasta'
+    elif inFX.lower().endswith('.fq') or inFX.lower().endswith('.fastq'):
+        informat = 'fastq'
     else:
-        print('Unknown file format for', outFX)
+        print('Unknown input file format for', inFX)
         print('Must be .fa/.fasta or .fq/.fastq')
         exit(1)
 
+    if outFX.lower().endswith('.fa') or outFX.lower().endswith('.fasta'):
+        outformat = 'fasta'
+    elif outFX.lower().endswith('.fq') or outFX.lower().endswith('.fastq'):
+        outformat = 'fastq'
+    else:
+        print('Unknown output file format for', outFX)
+        print('Must be .fa/.fasta or .fq/.fastq')
+        exit(2)
+
+    assert not exists(outFX), 'outFASTX already exists!'
+
     if ids is not None:
-        filterIDs(open(inFX, 'r'), open(ids, 'r'), open(outFX, 'w+'))
+        records, filtered, missed = filterIDs(inFX, informat, open(ids, 'r'))
+        print('Found Reads: ', len(records), ', Filtered:, ', len(filtered), ', Unseen IDs: ', len(missed))
 
-    elif long is not None:
-        records, longest, shortest = filterLength(inFX, long, 'long')
+    else:
+        if long is not None:
+            records, longest, shortest = filterLength(inFX, informat, long, 'long')
 
-    elif short is not None:
-        records, longest, shortest = filterLength(inFX, short, 'short')
+        elif short is not None:
+            records, longest, shortest = filterLength(inFX, informat, short, 'short')
+        print('longest', longest, 'shortest', shortest)
 
     for record in records:
         if dna:
@@ -63,23 +79,35 @@ def main() -> None:
         if rna:
             record.seq = record.seq.replace('T', 'U')
 
-    SeqIO.write(records, outFX, format)
-    print('longest', longest, 'shortest', shortest)
+    SeqIO.write(records, outFX, outformat)
 
-def filterLength(inFX : str, threshold : int, mode : str) -> tuple:
+def filterLength(inFX : str, format : str, threshold : int, mode : str) -> tuple:
+    '''
+    Filters the input FASTX for reads with a given length.
+    Filtering for shorter or longer reads is determined by the mode.
+
+    Parameters
+    ----------
+    inFX : str
+        input FASTX containing the reads
+    threshold : int
+        filter for this threshold
+    mode : str
+        filtering for 'long' or 'short' reads
+
+    Returns
+    -------
+    out : list
+        list of desired reads
+    longest : int
+        length of longest read in inFX
+    shortest : int
+        length of shortest read in inFX
+    '''
     func = {
         'long':lambda length, threshold: True if length >= threshold else False,
         'short':lambda length, threshold: True if length <= threshold else False
         }[mode]
-    
-    if inFX.lower().endswith('.fa') or inFX.lower().endswith('.fasta'):
-        format = 'fasta'
-    elif inFX.lower().endswith('.fq') or inFX.lower().endswith('.fastq'):
-        format = 'fastq'
-    else:
-        print('Unknown file format for', inFX)
-        print('Must be .fa/.fasta or .fq/.fastq')
-        exit(1)
 
     infx = SeqIO.parse(inFX, format)
     out = []
@@ -95,63 +123,56 @@ def filterLength(inFX : str, threshold : int, mode : str) -> tuple:
     print()
     return out, longest, shortest
 
-def filterIDs(inFX : TextIOWrapper, ids : TextIOWrapper, outFX : TextIOWrapper = None) -> tuple:
+def filterIDs(inFX : str, format : str, ids : TextIOWrapper) -> tuple:
     '''
-    Filters the input FASTA/FASTQ for ids in given list and writes filtered FASTA/FASTQ.
+    Filters the input FASTX for ids in given list and writes filtered FASTX.
     
     If outFX is not None:
         Writes found reads to outFX file.
 
     Parameters
     ----------
-    inFX : TextIOWrapper
+    inFX : str
         Input FASTA/FASTQ
-    ids : list
+    format : str
+        define the input file format
+    ids : TextIOWrapper
         ReadIDs to filter for
-    outFX : TextIOWrapper
-        Filtered output FASTA/FASTQ
 
     Returns
     -------
-    foundIDs : list
-        list of found IDs
-    filtered : list
+    foundRecords : list
+        list of found SeqRecords
+    removedIDs : list
         list of removed IDs
-    missingIDs : list
-        list of missing IDs
+    missedIDs : list
+        list of missed IDs
     '''
 
-    ids_list = list(map(lambda id : id[:-1] if '\n' in id else id, ids.readlines()))
-    writeRead = False
-    foundIDs = []
-    filteredIDs = []
+    ids_list = set(map(lambda id : id.strip(), ids.readlines()))
+    foundRecords = []
+    removedIDs = []
+
+    infx = SeqIO.parse(inFX, format)
 
     print(f'Looking for {len(ids_list)} ids')
 
-    for idx, line in enumerate(inFX, 1):
-        if idx%10000==0:
+    for idx, seq_record in enumerate(infx):
+        if (idx+1)%1000==0:
             print(f'Processing line {idx} ...', end='\r')
-        if line.startswith('@') or line.startswith('>'):
-            writeRead = False
-            readid = line.split()[0][1:]
-            if readid in ids_list:
-                writeRead = True
-                if outFX is not None:
-                    outFX.write(line)
-                ids_list.remove(readid)
-                foundIDs.append(readid)
-            else:
-                filteredIDs.append(readid)
+
+        if seq_record.name in ids_list:
+            foundRecords.append(seq_record)
+            ids_list.remove(seq_record.name)
         else:
-            if writeRead and outFX is not None:
-                outFX.write(line)
+            removedIDs.append(seq_record.name)
 
     print(f'Processed line {idx}\t\t')
-    print(f'Found {len(foundIDs)} ids')
-    print(f'Filtered {len(filteredIDs)} ids')
+    print(f'Found {len(foundRecords)} ids')
+    print(f'Filtered {len(removedIDs)} ids')
     print(f'Missed {len(ids_list)} ids')
 
-    return foundIDs, filteredIDs, ids_list
+    return foundRecords, removedIDs, ids_list
 
 if __name__ == '__main__':
     main()
